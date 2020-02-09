4387ec05845a4f3556918f7a2f0d9203f3e3f319
Vilen Kupriienko <pride2808@gmail.com>
2020-02-09 22:12:33 +0200

    Frontend was improved

--- public/js/buildUsersAndReceivers.js
+++ public/js/buildUsersAndReceivers.js
@@ -1,43 +1,53 @@
 function buildUsersAndReceivers(result) {
-	const usersContent = document.getElementById('login-list');
-	usersContent.innerHTML = '';
+  const usersContent = document.getElementById("login-list");
+  usersContent.innerHTML = "";
 
-	result.users.forEach(function(element, index) {
-		const div = document.createElement('div');
-		div.className = 'list';
-		div.id = element.login;
+  result.users.forEach(function(element, index) {
+    const div = document.createElement("div");
+    div.className =
+      "list-group-item d-flex justify-content-between align-items-center";
+    div.id = element.login;
 
-		const a = document.createElement('a');
-		a.href = 'admin/delete/user/' + element.login;
-		const span = document.createElement('span');
-		span.innerText = element.login;
-		const button = document.createElement('button');
-		button.innerText = '×';
+    const a = document.createElement("a");
+    a.href = "admin/delete/user/" + element.login;
+    const span = document.createElement("span");
+    span.innerText = element.login;
+    const button = document.createElement("button");
+    button.type = "button";
+    button.className = "btn btn-primary";
+    const button_cross = document.createElement("span");
+    button_cross.setAttribute("aria-hidden", "true");
+    button_cross.innerHTML = "&times;";
+    button.append(button_cross);
+    a.append(button);
+    div.append(span);
+    div.append(a);
+    usersContent.append(div);
+  });
 
-		a.append(button);
-		div.append(span);
-		div.append(a);
-		usersContent.append(div);
-	});
+  const receiversContent = document.getElementById("username-list");
+  receiversContent.innerHTML = "";
 
-	const receiversContent = document.getElementById('username-list');
-	receiversContent.innerHTML = '';
+  result.receivers.forEach(function(element, index) {
+    const div = document.createElement("div");
+    div.className =
+      "list-group-item d-flex justify-content-between align-items-center";
+    div.id = element.username;
 
-	result.receivers.forEach(function(element, index) {
-		const div = document.createElement('div');
-		div.className = 'list';
-		div.id = element.username;
-
-		const a = document.createElement('a');
-		a.href = '/admin/delete/receiver/' + element.username;
-		const span = document.createElement('span');
-		span.innerText = element.username;
-		const button = document.createElement('button');
-		button.innerText = '×';
-
-		a.append(button);
-		div.append(span);
-		div.append(a);
-		receiversContent.append(div);
-	});
+    const a = document.createElement("a");
+    a.href = "/admin/delete/receiver/" + element.username;
+    const span = document.createElement("span");
+    span.innerText = element.username;
+    const button = document.createElement("button");
+    button.type = "button";
+    button.className = "btn btn-primary";
+    const button_cross = document.createElement("span");
+    button_cross.setAttribute("aria-hidden", "true");
+    button_cross.innerHTML = "&times;";
+    button.append(button_cross);
+    a.append(button);
+    div.append(span);
+    div.append(a);
+    receiversContent.append(div);
+  });
 }
--- public/js/getInfoUsers.js
+++ public/js/getInfoUsers.js
@@ -1,5 +1,5 @@
 async function getAll() {
-	const response = await fetch('/admin/get');
-	const result = await response.json();
-	return result;
+  const response = await fetch('/admin/get');
+  const result = await response.json();
+  return result;
 }
