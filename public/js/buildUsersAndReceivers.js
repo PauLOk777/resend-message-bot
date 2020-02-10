e4978109c191df6dac0bb2c70020a7f12f9d0a20
PauLOk777 <trocjuk2000@gmail.com>
2020-02-10 20:46:06 +0200

    Admin can't see his profile in admin panel(for secure).

--- public/js/buildUsersAndReceivers.js
+++ public/js/buildUsersAndReceivers.js
@@ -3,26 +3,29 @@ function buildUsersAndReceivers(result) {
   usersContent.innerHTML = "";
 
   result.users.forEach(function(element, index) {
-    const div = document.createElement("div");
-    div.className =
-      "list-group-item d-flex justify-content-between align-items-center";
-    div.id = element.login;
+      if (element != 'Admin in panel') {
 
-    const a = document.createElement("a");
-    a.href = "admin/delete/user/" + element.login;
-    const span = document.createElement("span");
-    span.innerText = element.login;
-    const button = document.createElement("button");
-    button.type = "button";
-    button.className = "btn btn-primary";
-    const button_cross = document.createElement("span");
-    button_cross.setAttribute("aria-hidden", "true");
-    button_cross.innerHTML = "&times;";
-    button.append(button_cross);
-    a.append(button);
-    div.append(span);
-    div.append(a);
-    usersContent.append(div);
+      const div = document.createElement("div");
+      div.className =
+        "list-group-item d-flex justify-content-between align-items-center";
+      div.id = element.login;
+
+      const a = document.createElement("a");
+      a.href = "admin/delete/user/" + element.login;
+      const span = document.createElement("span");
+      span.innerText = element.login;
+      const button = document.createElement("button");
+      button.type = "button";
+      button.className = "btn btn-primary";
+      const button_cross = document.createElement("span");
+      button_cross.setAttribute("aria-hidden", "true");
+      button_cross.innerHTML = "&times;";
+      button.append(button_cross);
+      a.append(button);
+      div.append(span);
+      div.append(a);
+      usersContent.append(div);
+    }
   });
 
   const receiversContent = document.getElementById("username-list");
